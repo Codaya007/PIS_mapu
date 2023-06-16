4b4fe6c08782421390fb4effd6b801974ef17a73
Daniel21004 <jorsh506@hotmail.com>
2023-06-16 18:43:59 -0500

    feat: Add event model and event validationSchema

---
+++ api/src/models/Event.js
@@ -0,0 +1,30 @@
+const { Double } = require("mongodb");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    sinceDate: {
+        type: Date,
+        required: true
+    },
+    untilDate: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    price: {
+        type: Double,
+        required: true
+    },
+});
+
+const Event = mongoose.model("Event", eventSchema);
+
+module.exports = Event;
