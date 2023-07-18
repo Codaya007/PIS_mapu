f7dc81482932876a2187dac2f9123242be0695c2
Daniel21004 <jorsh506@hotmail.com>
2023-07-17 22:03:58 -0500

    feat: slidebar

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -2,14 +2,57 @@ import { NativeBaseProvider } from "native-base";
 import { NativeRouter } from "react-router-native";
 import Main from "./src/screens/Main";
 import Toast from 'react-native-toast-message';
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from "./src/screens/Home";
+import Events from "./src/screens/Events";
+import LoginForm from "./src/screens/Login";
+import Register from "./src/screens/Register";
+import About from "./src/screens/About";
+import Favorites from "./src/screens/Favorites";
+import ForgotPasswordForm from "./src/screens/ForgotPasswordForm";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
+  // <Main />
   return (
     <NativeBaseProvider>
       <NativeRouter>
-        <Main />
+        <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Home} />
+            <Drawer.Screen name="Events" component={Events} />
+            <Drawer.Screen name="Favorites" component={Favorites} />
+            <Drawer.Screen name="Login" component={LoginForm} />
+            <Drawer.Screen name="Register" component={Register} />
+            <Drawer.Screen name="ForgotPasswordForm" component={ForgotPasswordForm} />
+            <Drawer.Screen name="About" component={About} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+        <Toast />
       </NativeRouter>
-      <Toast/>
     </NativeBaseProvider>
   );
 }
--- mobile-client/src/screens/Main.jsx
+++ mobile-client/src/screens/Main.jsx
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 import React from "react";
 import AppBar from "../components/AppBar";
+import DrawerBar from "../components/DrawerBar";
 import { Route, Routes } from "react-router-native";
 import Home from "./Home";
 import LoginForm from "./Login";
@@ -13,20 +14,19 @@ import ForgotPasswordForm from "./ForgotPasswordForm";
 const Main = () => {
   return (
     // Aquí el flex: 1 me ayuda a que pueda hacer el scroll hasta abajo
-    <View style={{ flex: 1 }}>
-      <AppBar />
-      <Routes>
-        <Route exact path="/" Component={Home} />
-        <Route path="/login" Component={LoginForm} />
-        <Route path="/sign-up" Component={Register} />
-        <Route path="/about" Component={About} />
-        <Route path="/events" Component={Events} />
-        <Route path="/favorites" Component={Favorites} />
-        <Route path="/forgot-password" Component={ForgotPasswordForm} />
-        <Route path="/reset-password/:token" Component={ForgotPasswordForm} />
-        <Route path="/*" Component={Home} />
-      </Routes>
-    </View>
+      <View style={{ flex: 1 }}>
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route path="/login" Component={LoginForm} />
+          <Route path="/sign-up" Component={Register} />
+          <Route path="/about" Component={About} />
+          <Route path="/events" Component={Events} />
+          <Route path="/favorites" Component={Favorites} />
+          <Route path="/forgot-password" Component={ForgotPasswordForm} />
+          <Route path="/reset-password/:token" Component={ForgotPasswordForm} />
+          <Route path="/*" Component={Home} />
+        </Routes>
+      </View>
   );
 };
 
