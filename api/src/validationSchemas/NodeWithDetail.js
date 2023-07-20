f74b53bcbafc71789041227431eb7d0182cb5d60
vivi <viviana.calva@lumation.co>
2023-07-20 16:54:58 -0500

    Fix refactoring detail schema

--- api/src/validationSchemas/NodeWithDetail.js
+++ api/src/validationSchemas/NodeWithDetail.js
@@ -6,23 +6,37 @@ const {
   updateNodeSchema,
 } = require("./Node");
 
+const createDetailSchema = Joi.object({
+  title: Joi.string().required().max(50).messages({
+    "*": "El campo 'title' es requerido y debe tener hasta 50 caracteres",
+  }),
+  description: Joi.string().required().max(150).messages({
+    "*": "El campo 'description' es requerido y debe tener hasta 150 caracteres",
+  }),
+  img: Joi.string().required().uri().messages({
+    "*": "El campo 'img' es requerido y debe ser una url válida",
+  }),
+});
+
+const updateDetailSchema = Joi.object({
+  _id: Joi.string().required().messages({
+    "*": "El campo '_id' del detalle es requerido",
+  }),
+  title: Joi.string().optional().max(50).messages({
+    "*": "El campo 'title' debe tener hasta 50 caracteres",
+  }),
+  description: Joi.string().optional().max(150).messages({
+    "*": "El campo 'description' debe tener hasta 150 caracteres",
+  }),
+  img: Joi.string().optional().uri().messages({
+    "*": "El campo 'img' debe ser una url válida",
+  }),
+});
+
 // Definir el esquema de validación para la creación de un Nodo de interes
 const createNodeWithDetailSchema = createNodeSchema
   .keys({
-    detail: Joi.object()
-      .required()
-      .keys({
-        title: Joi.string().required().max(50).messages({
-          "*": "El campo 'title' es requerido y debe tener hasta 50 caracteres",
-        }),
-        description: Joi.string().required().max(150).messages({
-          "*": "El campo 'description' es requerido y debe tener hasta 150 caracteres",
-        }),
-        img: Joi.string().required().uri().messages({
-          "*": "El campo 'img' es requerido y debe ser una url válida",
-        }),
-      })
-      .messages({ "*": "El campo detail es requerido" }),
+    detail: createDetailSchema.required(),
   })
   .external(validateCampus)
   .external(validateCategory);
@@ -30,22 +44,7 @@ const createNodeWithDetailSchema = createNodeSchema
 // Definir el esquema de validación para la actualización de un Nodo de interés
 const updateNodeWithDetailSchema = updateNodeSchema
   .keys({
-    detail: Joi.object()
-      .optional()
-      .keys({
-        _id: Joi.string().required().messages({
-          "*": "El campo '_id' del detalle es requerido",
-        }),
-        title: Joi.string().optional().max(50).messages({
-          "*": "El campo 'title' debe tener hasta 50 caracteres",
-        }),
-        description: Joi.string().optional().max(150).messages({
-          "*": "El campo 'description' debe tener hasta 150 caracteres",
-        }),
-        img: Joi.string().optional().uri().messages({
-          "*": "El campo 'img' debe ser una url válida",
-        }),
-      }),
+    detail: updateDetailSchema.optional(),
   })
   .external(validateCategory)
   .external(validateCampus);
@@ -53,4 +52,6 @@ const updateNodeWithDetailSchema = updateNodeSchema
 module.exports = {
   createNodeWithDetailSchema,
   updateNodeWithDetailSchema,
+  createDetailSchema,
+  updateDetailSchema,
 };
