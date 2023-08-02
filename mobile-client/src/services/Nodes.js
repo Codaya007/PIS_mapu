5e8bb8c17dbaac2e02ef52196bba7cb3fb56b723
LeoMaza <dalton.l.maza@unl.edu.ec>
2023-08-01 22:43:25 -0500

    feat user gps location

--- mobile-client/src/services/Nodes.js
+++ mobile-client/src/services/Nodes.js
@@ -1,8 +1,34 @@
 import axios from "axios";
 import { API_BASEURL } from "../constants";
+import * as Location from 'expo-location';
 
 export const getAllNodes = async () => {
   const { data } = await axios.get(`${API_BASEURL}/node/coordinates`);
   
   return { nodes: data?.results };
+};
+
+export const coordinatesGPS = async () => {
+  const { status } = await Location.requestForegroundPermissionsAsync();
+
+  if (status !== 'granted') {
+    console.log('Permission to access location was denied');
+    return {};
+  }
+
+  const location = await Location.getCurrentPositionAsync({});
+  //pueden usar esto para pintar la ubicación como un pin
+  const gpsPoint = {
+    _id: "123456789",
+    available: true,
+    color: "yellow",
+    coordinates: [location.coords.latitude.toString(), location.coords.longitude.toString()],
+    latitude: location.coords.latitude.toString(),
+    longitude: location.coords.longitude.toString(),
+    name: "Mi Ubicación",
+    type: "Mi Ubicación",
+    isPreselected: true
+  };
+
+  return gpsPoint;
 };
