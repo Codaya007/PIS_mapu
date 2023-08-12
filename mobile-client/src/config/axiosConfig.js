05eff81f38fa585030be214f01f86e56da9d0aa4
vivi <viviana.calva@lumation.co>
2023-08-11 19:59:30 -0500

    Adding auth

--- mobile-client/src/config/axiosConfig.js
+++ mobile-client/src/config/axiosConfig.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { store } from "../store/store";
+import { logoutAction } from "../store/actions/authActions";
 
 // Función para manejar el caso de éxito
 const onRequestSuccess = async (config) => {
@@ -11,4 +13,24 @@ const onRequestSuccess = async (config) => {
   return config;
 };
 
+const onRejectRequest = (error) => {
+  try {
+    const responseData = error?.response?.data;
+
+    // console.log({ responseData });
+
+    // Verificar si el error es debido a un token JWT expirado
+    if (responseData && responseData.code === "jwtExpired") {
+      // Realizar la acción de deslogueo aquí (por ejemplo, redireccionar a la página de inicio de sesión)
+      console.log("Token JWT expirado. Realizar deslogueo automático.");
+      store.dispatch(logoutAction());
+    }
+  } catch (err) {
+    console.log({ err });
+  }
+
+  return Promise.reject(error);
+};
+
 axios.interceptors.request.use(onRequestSuccess, (error) => {});
+axios.interceptors.response.use((response) => response, onRejectRequest);
